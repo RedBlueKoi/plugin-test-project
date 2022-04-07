5c1b18d678eedc68ea152bee154642f42e0832e7
RedBlueKoi <bragin.inred@gmail.com>
2022-04-07 04:18:10 +0200

    feat: enabling the mock server

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ import router from "./router"
 
 import makeServer from "./server"
 
-if (process.env.NODE_ENV === "development") {
-  makeServer()
-}
+// if (process.env.NODE_ENV === "development") {
+makeServer()
+// }
 
 const app = createApp(App)
 
